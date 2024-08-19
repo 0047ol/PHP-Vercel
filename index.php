bc906058a9dd92744606223839f79ac495690011
0047ol <i0047ol@qq.com>
2024-08-19 17:49:02 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,2 @@
 <?php
-
-//phpinfo(INFO_ALL & ~INFO_ENVIRONMENT & ~INFO_CONFIGURATION & ~INFO_VARIABLES);
-header('Location: https://plus.androidide.cn');
-exit();
+phpinfo(INFO_ALL & ~INFO_ENVIRONMENT & ~INFO_CONFIGURATION & ~INFO_VARIABLES);
